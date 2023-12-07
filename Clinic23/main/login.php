e7e25278151b30653d93747213fb3abfbbcfc3da
Ryan Shirlock <ryashirl@gmail.com>
2023-12-06 19:04:11 -0500

    More PHP Code

--- Clinic23/main/login.php
+++ Clinic23/main/login.php
@@ -25,7 +25,7 @@
   </div>
 </form>
 <?php
-    $db = $dbconnect = pg_connect("host=lemuria dbname=medical_server user=cis4250 password=blibber");
+    $db = new mysqli('lemuria', 'cis4250', 'medical_server', 'blibber');
     if ($db->connect_errno > 0){
       echo "<p>Error: Could not connect to database.<br></p>";
       exit;
--- Clinic23/main/patientInfo.php
+++ Clinic23/main/patientInfo.php
@@ -1,5 +1,5 @@
 <!-- Created by: Ryan Shirlock -->
-<!-- Modified: 12/05/23 -->
+<!-- Modified: 12/06/23 -->
 
 <?php
 $servername = "lemuria";
@@ -18,8 +18,9 @@ try {
     foreach($connection->query($query) as $row) {
         echo "Name: ";
         print_r($row["first_name"]);
+        echo " ";
         print_r($row["last_name"]);
-        echo "<br/>Gender/Sex: ";
+        echo "<br/>Gender: ";
         print_r($row["gender"]);
         echo "<br/>Date of Birth: ";
         print_r($row["DOB"]);
@@ -36,28 +37,6 @@ try {
         print_r($row["state"]);
         echo ", ";
         print_r($row["zip_code"]);
-        echo "
-        <b>Contacts: </b>
-        <b>#1: </b>
-        <P> - Thomas Shenkov {Emergency} </p>
-        <b>Phone #: </b>
-        <p> 802-369-4269 </p>
-        <b>Relation: </b>
-        <p> Family Member </p>
-        <b>#2: </b>
-        <p> - Noah Woodard </p>
-        <b>Phone #: </b>
-        <p> 802-976-2834 </p>
-        <b>Relation: </b>
-        <p> Doctor </p> <br/>
-        
-        <b>Insurance Provider: </b>
-        <p> VGA Insurance </p>
-        <b>Last Check-Up: </b>
-        <p> 04/20/2022 </p>
-        <b>Last Physical: </b>
-        <p> 07/30/2023 </p> <br/>"
-
     echo "End of info.<br/>";
 //I say ‘end of list’ so that if nothing is found, something is still printed
     $connection = null;
@@ -67,38 +46,3 @@ catch (PDOException $e) {
     die();
 }
 ?>
-<!--
-        <b>Name: </b>
-        <p> Auditore, David </p>
-        <b>DOB: </b>
-        <p> 09/12/2013 </p>
-        <b>Height: </b>
-        <p> 5'6"</p>
-        <b>Weight: </b>
-        <p> 170lbs </p> <br/>
-        
-        <b>Contacts: </b>
-        <b>#1: </b>
-        <P> - Thomas Shenkov {Emergency} </p>
-        <b>Phone #: </b>
-        <p> 802-369-4269 </p>
-        <b>Relation: </b>
-        <p> Family Member </p>
-        <b>#2: </b>
-        <p> - Noah Woodard </p>
-        <b>Phone #: </b>
-        <p> 802-976-2834 </p>
-        <b>Relation: </b>
-        <p> Doctor </p> <br/>
-        
-        <b>Insurance Provider: </b>
-        <p> VGA Insurance </p>
-        <b>Last Check-Up: </b>
-        <p> 04/20/2022 </p>
-        <b>Last Physical: </b>
-        <p> 07/30/2023 </p> <br/>
-        
-    </p>
-    <b> Patient Info:</b>
-    <p> The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start. </p>
--->
