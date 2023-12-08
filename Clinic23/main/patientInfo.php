54ad3f0910082e45be867cbe12fdb3f64c77ebf8
Ryan Shirlock <ryashirl@gmail.com>
2023-12-08 12:18:05 -0500

    updated php

--- Clinic23/main/patientInfo.php
+++ Clinic23/main/patientInfo.php
@@ -1,6 +1,15 @@
 <!-- Created by: Ryan Shirlock -->
 <!-- Modified: 12/06/23 -->
 
+<!DOCTYPE html>
+<html>
+    
+	<head>
+		<title> Northern Psychiatric Services </title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+    <body>
+    
 <?php
 $servername = "lemuria.cis.vermontstate.edu";
 $username   = "cis4250";
@@ -52,4 +61,30 @@ catch (PDOException $e) {
     print "Error: " . $e->getMessage() . "<br/>";
     die();
 }
-?>
+?>  
+        <b>Contacts: </b>
+        <b>#1: </b>
+        <P> - Thomas Shenkov {Emergency} </p>
+        <b>Phone #: </b>
+        <p> 802-369-4269 </p>
+        <b>Relation: </b>
+        <p> Family Member </p>
+        <b>#2: </b>
+        <p> - Noah Woodard </p>
+        <b>Phone #: </b>
+        <p> 802-976-2834 </p>
+        <b>Relation: </b>
+        <p> Doctor </p> <br/>
+        
+        <b>Insurance Provider: </b>
+        <p> VGA Insurance </p>
+        <b>Last Check-Up: </b>
+        <p> 04/20/2022 </p>
+        <b>Last Physical: </b>
+        <p> 07/30/2023 </p> <br/>
+        <b> Patient Info:</b>
+        <p> The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start. </p>
+    
+    </body>
+
+</html>
